bf0dab0d058db6cb451b66fd9e846bd8484a071d
Nadya <pakusha90@mail.ru>
2022-07-23 00:38:50 +0300

    pluggin description to API

--- src/app/home/description/description.component.ts
+++ src/app/home/description/description.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from '../pokemon.service';
+import { take } from 'rxjs';
+import { Pokemon } from 'src/app/shared/pokemon.model';
 
 @Component({
   selector: 'app-description',
@@ -8,12 +10,19 @@ import { PokemonService } from '../pokemon.service';
   styleUrls: ['./description.component.scss']
 })
 export class DescriptionComponent implements OnInit {
-  id:number;
+  pokemon: Pokemon;
+  id: string;
   constructor(private route: ActivatedRoute, private pokemonSrv: PokemonService) { }
 
   ngOnInit(): void {
     let paramId = this.route.snapshot.paramMap.get('id');
-    paramId ? this.id = +paramId: 0;
+
+    if (paramId) {
+      this.id = paramId;
+      this.pokemonSrv.getPokemonsById(this.id).pipe(take(1)).subscribe(res => {
+        this.pokemon = res;
+      })
+    }
 
 
   }
